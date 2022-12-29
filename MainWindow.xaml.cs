ef02c2c7ba8976b838693654fe03e7010ff2eb80
Teoeo123 <82089406+Teoeo123@users.noreply.github.com>
2022-12-29 18:30:54 +0100

    Dodaj pliki projektów.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Diagnostics;
+
+namespace TwitchChatReader_v3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        private void StackPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+
+        private void ClickRed(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                Close();
+        }
+
+        private void ClickGreen(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left && WindowState==WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+               
+            }
+            else
+            {
+                WindowState = WindowState.Maximized;
+            }
+
+        }
+        private void ClickYellow(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                WindowState = WindowState.Minimized;
+        }
+        
+        private void ClickBlack(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left && searchbar.Visibility == Visibility.Visible)
+            {
+                searchbar.Visibility = Visibility.Hidden;
+            }
+            else
+                searchbar.Visibility = Visibility.Visible;
+        }
+
+        private void Search(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Trace.WriteLine("enter");
+                searchbar.Visibility = Visibility.Hidden;
+                StackChat.Visibility = Visibility.Visible;
+                WSlabel.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
